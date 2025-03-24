1be5cb93832cfa2fbcb5832dd9627155daf071a5
老村翼 <oimurat@systena.co.jp>
2025-03-24 14:38:31 +0900

    test:Jestの動作確認(異常系)

--- react/src/App.test.tsx
+++ react/src/App.test.tsx
@@ -6,11 +6,11 @@ import App from './App';
  * サンプルテスト
  */
 describe('サンプルテスト', () => {
-    test('[正常系]サンプルテスト', async () => {
+    test('[異常系]サンプルテスト', async () => {
         // 実行
         render(<App />);
 
         // 検証
-        expect(screen.getByText('Vite')).toBeInTheDocument();
+        expect(screen.getByText('React')).toBeInTheDocument();
     });
 });
