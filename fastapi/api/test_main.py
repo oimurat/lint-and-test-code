f5627148b44438e1459ca2374942baa118482754
老村翼 <oimurat@systena.co.jp>
2025-03-24 16:08:26 +0900

    fix:test_main.pyの修正

--- fastapi/api/test_main.py
+++ fastapi/api/test_main.py
@@ -3,8 +3,7 @@ from main import app
 
 client = TestClient(app)
 
-
-def test_read_main():
+def test_read_main() -> None:
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
